d8fa934dffcc679ed5cb6682f7e7022b5cf42d63
stmfusijhb024 <stmfusijhb024@student.wethinkcode.co.za>
2024-11-02 12:34:28 +0200

    finished with this project

--- digital_clock.py
+++ digital_clock.py
@@ -5,6 +5,12 @@ import time
 
 root =  Tk()
 root.title('Display Clock')
-clock = 
 
+def time_display():
+    display_time = time.strftime('%H:%M:%S %p')
+    clock.config(text = display_time)
+    clock.after(180, time_display)
+clock = Label(root, font= ('impact', 16), bg= 'blue',fg = 'red')
+clock.pack()
+time_display()
 root.mainloop()
